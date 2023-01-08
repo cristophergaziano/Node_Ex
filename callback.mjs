2fce2518921c0b439b26a454a3b46ab456c4216b
Cristopher <cristopher.gaziano@gmail.com>
2023-01-08 14:43:53 +0100

    ex 11 done

--- callback.mjs
+++
@@ -1,8 +0,0 @@
-import { writeFile } from 'node:fs';
-import { Buffer } from 'node:buffer';
-
-const data = new Uint8Array(Buffer.from('Ciao, sono stato creato tramite uno script, e tu?'));
-writeFile('messaggio.txt', data, (error) => {
-  if (error) throw error;
-  console.log('Il file "messaggio.txt" è stato correttamente creato!')
-});
